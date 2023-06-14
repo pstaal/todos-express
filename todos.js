d18221b7c282106895e6c532dec30634b77678ea
peter staal <peterstaal@MBP-van-peter.home>
2023-06-14 13:18:52 +0200

    add sorting for todos

--- todos.js
+++ todos.js
@@ -14,9 +14,35 @@ app.set("view engine", "pug");
 app.use(morgan("common"));
 app.use(express.static("public"));
 
+// Compare todo list titles alphabetically
+const compareByTitle = (todoListA, todoListB) => {
+    let titleA = todoListA.title.toLowerCase();
+    let titleB = todoListB.title.toLowerCase();
+  
+    if (titleA < titleB) {
+      return -1;
+    } else if (titleA > titleB) {
+      return 1;
+    } else {
+      return 0;
+    }
+};
+  
+// return the list of todo lists sorted by completion status and title.
+const sortTodoLists = lists => {
+    let undone = lists.filter(todoList => !todoList.isDone());
+    let done   = lists.filter(todoList => todoList.isDone());
+    undone.sort(compareByTitle);
+    done.sort(compareByTitle);
+    return [].concat(undone, done);
+};
+
+// Render the list of todo lists
 app.get("/", (req, res) => {
-    res.render("lists", { todoLists });
-});
+    res.render("lists", {
+      todoLists: sortTodoLists(todoLists),
+    });
+  });
 
 // Listener
 app.listen(port, host, () => {
