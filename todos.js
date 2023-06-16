8c1f477adee14992c91ee0de3f35cb23b2c5bd4b
peter staal <peterstaal@MBP-van-peter.home>
2023-06-16 12:29:11 +0200

    add individual pages for todolists

--- todos.js
+++ todos.js
@@ -3,7 +3,9 @@ const morgan = require("morgan");
 const flash = require("express-flash");
 const session = require("express-session");
 const { body, validationResult } = require("express-validator");
+
 const TodoList = require("./lib/todolist");
+const { sortTodoLists, sortTodos } = require("./lib/sort");
 
 const app = express();
 const host = "localhost";
@@ -34,27 +36,10 @@ app.use((req, res, next) => {
     next();
 });
 
-// Compare todo list titles alphabetically
-const compareByTitle = (todoListA, todoListB) => {
-    let titleA = todoListA.title.toLowerCase();
-    let titleB = todoListB.title.toLowerCase();
-  
-    if (titleA < titleB) {
-      return -1;
-    } else if (titleA > titleB) {
-      return 1;
-    } else {
-      return 0;
-    }
-};
-  
-// return the list of todo lists sorted by completion status and title.
-const sortTodoLists = lists => {
-    let undone = lists.filter(todoList => !todoList.isDone());
-    let done   = lists.filter(todoList => todoList.isDone());
-    undone.sort(compareByTitle);
-    done.sort(compareByTitle);
-    return [].concat(undone, done);
+// Find a todo list with the indicated ID. Returns `undefined` if not found.
+// Note that `todoListId` must be numeric.
+const loadTodoList = todoListId => {
+  return todoLists.find(todoList => todoList.id === todoListId);
 };
 
 // Render the list of todo lists
@@ -104,6 +89,29 @@ app.post("/lists",
     }
   }
 );
+
+// Render individual todo list and its todos
+app.get("/lists/:todoListId", (req, res, next) => {
+  let todoListId = req.params.todoListId;
+  let todoList = loadTodoList(+todoListId);
+  if (todoList === undefined) {
+    next(new Error("Not found."));
+  } else {
+    res.render("list", {
+      todoList: todoList,
+      todos: sortTodos(todoList),
+    });
+  }
+});
+
+// Error handler
+app.use((err, req, res, _next) => {
+  console.log(err); // Writes more extensive information to the console log
+  res.status(404).send(err.message);
+});
+
+
+
 // Listener
 app.listen(port, host, () => {
   console.log(`Todos is listening on port ${port} of ${host}!`);
